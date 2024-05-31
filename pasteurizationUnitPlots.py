63f4eb01a2ba9293636a6043dbb8bf19c10b6735
alexponcho64 <alexponcho99@gmail.com>
2024-05-31 08:58:17 +0200

    Update pasteurizationUnitPlots.py

--- pasteurizationUnitPlots.py
+++ pasteurizationUnitPlots.py
@@ -8,10 +8,6 @@ st.title("Digital Twin for a Laboratory Pasteurization Unit")
 
 # Directory where the Excel files are located
 data_dir = "data"
-<<<<<<< HEAD
-=======
-
->>>>>>> temp-branch
 
 files = {
     "Individual step responses": "ptc23_steps_1.xlsx",
